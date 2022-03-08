12cc29dd780e4d5195d829d587d1bdf6f72b6000
8012574-lance-ruiz <8012574@jeffersonunion.net>
2022-03-07 20:28:16 -0800

    scrolling - updown #1

--- main.js
+++ main.js
@@ -1,14 +1,28 @@
-$(document).ready(function() {
-    
-    $(window).scroll( function(){  // Listen to every time the window is scrolled 
+let html = document.querySelector(`html`);
+let scrollPosition = window.scrollY;
 
-        $('.fadeable').each( function(){ // Checks the location of each desired element with the 'fadeable' id
+document.addEventListener("DOMContentLoaded", function() {
+    document.getElementById("user-greeting").textContent = "Welcome back, Bart";
+});
+
+window.addEventListener(`scroll`, () => {
+    scrollPosition = window.scrollY;
+    console.log(scrollPosition);
+})
+
+/*
+{ // Event listener; triggers only once after the page loads
+    
+    window.scroll(function(){  // Event listener; triggers every time the window is scrolled 
+
+        $('.fadeable').each(function(){ // Checks the location of each desired element with the 'fadeable' id
             
-            var bottom_of_object = $(this).position().top + $(this).outerHeight();
-            var bottom_of_window = $(window).scrollTop() + $(window).height();
+            let bottom_of_object = this.position().top + this.outerHeight();
+            let bottom_of_window = window.scrollTop() + window.height();
+            print('g')
             
-            if( bottom_of_window > bottom_of_object ) {
-                $(this).animate({'opacity':'1'}, 1500);
+            if(bottom_of_window > bottom_of_object) {
+                this.animate({'opacity': '1'}, 5000);
             }
             
         }); 
@@ -16,3 +30,4 @@ $(document).ready(function() {
     });
     
 });
+*/
