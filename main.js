bf3e394a50aa2c4b48641ca68e7d5fb91a3daa97
8012574-lance-ruiz <8012574@jeffersonunion.net>
2022-03-08 11:41:25 -0800

    scrolling - updown #2

--- main.js
+++ main.js
@@ -1,14 +1,24 @@
 let html = document.querySelector(`html`);
 let scrollPosition = window.scrollY;
 
-document.addEventListener("DOMContentLoaded", function() {
-    document.getElementById("user-greeting").textContent = "Welcome back, Bart";
+document.addEventListener(`DOMContentLoaded`, function() {
+    const fadeables = document.getElementsByClassName(`fadeable`)
+    console.log(fadeables)
+
+    for (let i = 0; i < fadeables.length; i++) {
+        const element = fadeables[i]
+        console.log(element)
+        console.log(i)
+    }
+    /*fadeables.forEach(function(item) { wtf is an HTMLCollection rip arrays
+        console.log(item)
+    })*/
 });
 
 window.addEventListener(`scroll`, () => {
-    scrollPosition = window.scrollY;
+    scrollPosition = window.scrollY; 
     console.log(scrollPosition);
-})
+});
 
 /*
 { // Event listener; triggers only once after the page loads
@@ -19,7 +29,6 @@ window.addEventListener(`scroll`, () => {
             
             let bottom_of_object = this.position().top + this.outerHeight();
             let bottom_of_window = window.scrollTop() + window.height();
-            print('g')
             
             if(bottom_of_window > bottom_of_object) {
                 this.animate({'opacity': '1'}, 5000);
